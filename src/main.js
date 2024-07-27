3f739904937f4d8f1af19106965c94f55f6fa7c8
Joaquin Hernandez Martinez <proyecto_noether@outlook.com>
2024-07-27 17:16:16 +0200

    Most changes done for video dialog

--- src/main.js
+++ src/main.js
@@ -1,10 +1,34 @@
+import '../styles/modern-normalize.css';
+import '../styles/style.css';
+import '../styles/components/header.css';
+import '../styles/components/hero.css';
+import '../styles/components/about.css';
+import '../styles/components/featured.css';
+import '../styles/components/work.css';
+import '../styles/components/contact.css';
+import '../styles/components/footer.css';
+import '../styles/components/mobile-nav.css';
+import '../styles/utils.css';
+
+import mobileNav from './utils/mobile-nav';
+import darkMode from './utils/dark-mode';
+import lazyLoading from './utils/lazy-loading';
+
+mobileNav();
+darkMode();
+lazyLoading();
+
 document.getElementById('openDialogBtn').addEventListener('click', function() {
-    document.body.classList.add('dimmed');
-    const dialog = document.getElementById('videoDialog');
-    dialog.classList.add('show');
-    setTimeout(() => {
-        dialog.style.display = 'flex';
-    }, 10); // slight delay to ensure transition effect
+    if (window.innerWidth > 475) {
+        document.body.classList.add('dimmed');
+        const dialog = document.getElementById('videoDialog');
+        dialog.classList.add('show');
+        setTimeout(() => {
+            dialog.style.display = 'flex';
+        }, 10); // slight delay to ensure transition effect
+    } else {
+        window.open('https://vimeo.com/806713496', '_blank');
+    }
 });
 
 document.getElementById('closeDialogBtn').addEventListener('click', function() {
